7ee23d1ad2087a3d7d74381e668b61e7eff40e0c
Kelly Nelson <kelly.nelson@e-bydesign.ca>
2023-06-20 20:50:38 -0700

    add contact form page and menu item'

---
+++ pages/contact.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h2>Contact</h2>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-form
+          name="FFP Contact Form"
+          method="post"
+          action="/thanks"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          @submit="submit($event)"
+        >
+          <v-text-field
+            v-model="name"
+            label="Name"
+            persistent-placeholder
+            density="compact"
+          ></v-text-field>
+          <v-text-field
+            v-model="emailAddress"
+            label="Email Address"
+            persistent-placeholder
+            density="compact"
+          ></v-text-field>
+          <v-textarea
+            v-model="text"
+            label="Talk to us"
+            persistent-placeholder
+          ></v-textarea>
+          <v-btn color="primary" type="submit" block class="mt-2 mb-4">
+            <span v-if="!loading">Submit</span>
+            <v-progress-circular
+              v-if="loading"
+              indeterminate
+              model-value="20"
+            ></v-progress-circular>
+          </v-btn>
+          <input value="FFP Contact Form" name="form-name" type="hidden" />
+          <p hidden>
+            <label>Don’t fill this out: <input name="bot-field" /></label>
+          </p>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script setup lang="ts">
+const name = ref(null);
+const emailAddress = ref(null);
+const text = ref(null);
+const loading = ref(false);
+
+function submit(e) {
+  e.preventDefault();
+}
+</script>
