7e3d9332a1e1133c46e0df36f3e6eda489ae8293
Kelly Nelson <kelly.nelson@e-bydesign.ca>
2023-06-25 21:49:05 -0700

    Add meta tags

--- app.vue
+++ app.vue
@@ -9,3 +9,26 @@
     </v-app>
   </div>
 </template>
+
+<script setup lang="ts">
+// useHead({
+//   title: "TEST Friends Feast Plans",
+//   meta: [
+//     {
+//       name: "description",
+//       content:
+//         "TEST Quickly know where the different age groups will be attending the Feast of Tabernacles",
+//     },
+//   ],
+// });
+
+useSeoMeta({
+  title: "Friends Feast Plans",
+  ogTitle: "Friends Feast Plans",
+  description:
+    "Quickly know where the different age groups will be attending the Feast of Tabernacles",
+  ogDescription:
+    "Quickly know where the different age groups will be attending the Feast of Tabernacles",
+  ogImage: "https://friendsfeastplans.org/images/FFP_Tent_LOGO_720x440.png",
+});
+</script>
